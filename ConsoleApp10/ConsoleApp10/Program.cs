4698a9f6dd08d8e897baac193d8d167973d37bb8
mrmines85 <61034810+mrmines85@users.noreply.github.com>
2020-02-25 18:35:44 -0600

    Add files via upload
    Lab 4.6 MarcusM

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = new string[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            string[] tasks = new string[7];
+
+            for (int x = 0; x < days.Length; x++)
+            {
+
+                Console.WriteLine("Type in a new Task for " + days[x]);
+                tasks[x] = days[x] + " : " + Console.ReadLine();
+
+            }
+
+            for (int x = 0; x < days.Length; x++)
+            {
+                Console.WriteLine(tasks[x]);
+            }
+        }
+    }
+}
